656a1c47bbca36712d95a04739fc50ad4d0a58b7
LAlex <lalex@lalex.com>
2023-03-24 16:24:19 +0000

    Bug report

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-(async () => {
-    const fast = require("fastify");
-    console.log("fast=", typeof fast);
-    console.log("require=", require.name);
-    console.log("require.cache=", typeof require.cache);
-})()
+const fast = require("fastify");
+console.log("fast=", typeof fast);
+console.log("require=", require.name);
+console.log("require.cache=", typeof require.cache);
